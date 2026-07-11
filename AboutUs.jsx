af2843e50dd2713fa0db0873d6f3986697a1f732
hoanglee-sys <vuminhh291@gmail.com>
2026-07-11 18:15:21 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery! We are dedicated to bringing nature into your home with our wide variety of beautiful, high-quality houseplants. Our mission is to provide the best greenery to purify your air, brighten your day, and create a relaxing environment.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
